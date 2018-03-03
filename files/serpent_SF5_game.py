5cddd5719e1dc1f0d60bf49ddd043552f5fd7649
m1ndgames <m1ndfuck@m1ndfuck.de>
2018-03-03 19:13:02 +0100

    redo

--- files/serpent_SF5_game.py
+++ files/serpent_SF5_game.py
@@ -14,28 +14,32 @@ class SerpentSF5Game(Game, metaclass=Singleton):
         kwargs["window_name"] = "StreetFighterV"
 
         kwargs["app_id"] = 310950
-        kwargs["app_args"] = {"windowed":None,"ResX=640":None,"ResY=480":None,"NOSPLASH":None}
+        kwargs["app_args"] = {"windowed":None,"ResX=1280":None,"ResY=720":None,"NOSPLASH":None}
 
         super().__init__(**kwargs)
 
         self.api_class = SF5API
         self.api_instance = None
 
-        self.frame_transformation_pipeline_string = "FLOAT"
+        self.frame_transformation_pipeline_string = "RESIZE:100x100|GRAYSCALE|FLOAT"
 
     @property
     def screen_regions(self):
         regions = {
-            "MENU_BUTTON_START": (283, 252, 297, 388),
-            "MENU_BUTTON_TRAINING": (282, 101, 319, 258),
-            "FIGHTZONE": (113, 1, 390, 639),
-            "TIMER": (100, 299, 71, 341),
-            "P1_HEALTH": (77, 297, 93, 51),
-            "P1_TRIGGER": (405, 129, 399, 57),
-            "P1_CA": (409, 71, 418, 185),
-            "P2_HEALTH": (77, 341, 93, 590),
-            "P2_TRIGGER": (399, 582, 405, 512),
-            "P2_CA": (409, 567, 418, 456)
+            "MENU_BUTTON_START": (475, 505, 445, 775),
+            "MENU_BUTTON_TRAINING": (517, 203, 444, 516),
+            "MENU_STAGESELECT": (110, 780, 84, 500),
+            "MENU_PLAYERSELECT": (449, 818, 424, 462),
+            "FIGHTZONE": (109, 1, 657, 1279),
+            "TIMER": (28, 683, 76, 597),
+            "P1_HEALTH": (36, 594, 63, 102),
+            "P2_HEALTH": (63, 685, 37, 1178),
+            "P1_TRIGGER": (691, 259, 678, 114),
+            "P2_TRIGGER": (690, 1021, 678, 1180),
+            "P1_CA": (715, 369, 697, 143),
+            "P2_CA": (715, 910, 698, 1136),
+            "P1_DIZZY": (79, 446, 68, 594),
+            "P2_DIZZY": (67, 833, 80, 685),
         }
 
         return regions
